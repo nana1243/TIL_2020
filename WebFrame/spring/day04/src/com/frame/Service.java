aa45d26ec1d2ba9ac8a5a23545855ba530b4c42f
nana1243 <nana1243@github.com>
2019-12-15 15:32:20 +0900

    add webframe folder such as Spring folder

---
+++ WebFrame/spring/day04/src/com/frame/Service.java
@@ -0,0 +1,18 @@
+package com.frame;
+
+import java.util.ArrayList;
+
+import org.springframework.transaction.annotation.Transactional;
+
+// db변경이 될때 transactional~
+
+public interface Service<K,V> {
+	@Transactional
+	public void register(V v) throws Exception;
+	@Transactional
+	public void remove(K k) throws Exception;
+	@Transactional
+	public void modify(V v) throws Exception;
+	public V get(K k) throws Exception;
+	public ArrayList<V> get() throws Exception;
+}
